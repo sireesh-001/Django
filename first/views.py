f55ec059f8128170403b321050dfb24b426f01c8
Sireesh reddy <koduru.reddy.19cse@bmu.edu.in>
2021-04-29 12:34:36 +0530

    Database

--- first/views.py
+++ first/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import first
 
 # Create your views here.
 def index(request):
-    return render(request, 'first/index.html')
+    items_1=first.objects.order_by('id')
+    context={"text_values":items_1}
+    return render(request, 'first/index.html',context)

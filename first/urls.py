e3f80070affe5d8582bec77ed23c25a0634b4cef
Sireesh reddy <koduru.reddy.19cse@bmu.edu.in>
2021-04-30 04:30:26 +0530

    completed

--- first/urls.py
+++ first/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('add',views.form_view,name='add'),
+    path('completed/<first_id>',views.completed,name='completed'),
+    path('delete',views.deleted,name='delete'),
+    path('deleteall',views.deleteall,name='deleteall')
 ]
--- first/views.py
+++ first/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import first
-
+from django.views.decorators.http import require_POST
+from .forms import firstform
 # Create your views here.
 def index(request):
     items_1=first.objects.order_by('id')
-    context={"text_values":items_1}
+    form=firstform
+    context={"text_values":items_1,'form':form}
     return render(request, 'first/index.html',context)
+
+@require_POST
+def form_view(request):
+    form_input=firstform(request.POST)
+    if form_input.is_valid():
+        first_list=first(text=request.POST['text'])
+        first_list.save()
+    return redirect('index')
+
+def completed(request,first_id):
+    items=first.objects.get(pk=first_id)
+    items.completed=True
+    items.save()
+
+    return redirect('index')
+
+def deleted(request):
+    first.objects.filter(completed__exact=True).delete()
+    return redirect('index')
+
+def deleteall(request):
+    first.objects.all().delete()
+    return redirect('index')
